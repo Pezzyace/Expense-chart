feac0ea8d3e59433ceb157206cd95a7bd4298728
Akaehomen peace <peacedavid049@gmail.com>
2023-09-30 00:40:21 +0100

    commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+const tooltip = document.querySelectorAll('tooltip');
+const bar = document.querySelectorAll('bar');
+
+bar.addEventListener("click", () => {
+  tooltip.style.display = 'block';
+})
+
